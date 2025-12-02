879d550decb9c96168701f5ab4810eba863f90b0
unknown <benoit.mosele@gmail.com>
2025-12-02 12:10:40 +0100

    feature/employee-routes

---
+++ back/routes/employeeRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import employeeController from "../controllers/employeeController.js";
+
+const router = express.Router();
+
+router.get('/allEmployees', employeeController.getAllEmployee);
+router.get('/employeesById/:id', employeeController.getEmployeeById);
+router.post('/addEmployee', employeeController.createEmployee);
+router.put('/updateEmployee', employeeController.updateEmployee);
+router.delete('/deleteEmployee/:id', employeeController.deleteEmployee);
+
+export default router;
