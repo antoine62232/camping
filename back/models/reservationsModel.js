1fea2e5b170950b70a2f255159857aea08baa730
antoine62232 <antoinebayart@laposte.net>
2025-12-03 14:58:18 +0100

    feature back-MCV-reservations

--- back/models/reservationsModel.js
+++ back/models/reservationsModel.js
@@ -1,23 +1,46 @@
 import connexion from "../config/bdd.js";
 
-<<<<<<< HEAD
-const createReservation = async (arrivalDateReservation, departureDateReservation, numberAdult, numberChildren, priceHtReservation, tvaReservation, priceTotal, statusReservation, userId) => {
+const createReservation = async (arrivalDateReservation, departureDateReservation, numberAdult, numberChildren, priceHtReservation, tvaReservation, priceTotal, statusReservation, userId, accommodationId) => {
     const insertReservation = `
-        INSERT INTO reservations (dateReservation, arrivalDateReservation, departureDateReservation, numberAdult, numberChildren, priceHtReservation, tvaReservation, priceTotal, statusReservation, userId)
-        VALUES (NOW(), ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        INSERT INTO reservations (dateReservation, arrivalDateReservation, departureDateReservation, numberAdult, numberChildren, priceHtReservation, tvaReservation, priceTotal, statusReservation, userId, accommodationId)
+        VALUES (NOW(), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
     `;
-    const [result] = await connexion.query(insertReservation, [arrivalDateReservation, departureDateReservation, numberAdult, numberChildren, priceHtReservation, tvaReservation, priceTotal, statusReservation, userId]);
-=======
-const createReservation = async (arrivalDateReservation, departureDateReservation, numberAdult, numberChildren, priceHtReservation, tvaReservation, priceTotal) => {
-    const insertReservation = `
-        INSERT INTO reservations (dateReservation, arrivalDateReservation, departureDateReservation, numberAdult, numberChildren, priceHtReservation, tvaReservation, priceTotal) 
-        VALUES (NOW(), ?, ?, ?, ?, ?, ?, ?);
-    `;
-    const [result] = await connexion.query(insertReservation, [arrivalDateReservation, departureDateReservation, numberAdult, numberChildren, priceHtReservation, tvaReservation, priceTotal]);
->>>>>>> baa04f5f1329b8af029ca593e1c9ea1ba0af01d9
+    const [result] = await connexion.query(insertReservation, [arrivalDateReservation, departureDateReservation, numberAdult, numberChildren, priceHtReservation, tvaReservation, priceTotal, statusReservation, userId, accommodationId]);
     return result.insertId;
 }
 
+const getAllReservations = async () => {
+    const selectAllReservations = `SELECT dateReservation, arrivalDateReservation, departureDateReservation, numberAdult, numberChildren, priceHtReservation, tvaReservation, priceTotal, statusReservation, userId, accommodationId FROM reservations`;
+    const [result] = await connexion.query(selectAllReservations);
+    return result;
+}
+
+const getReservationById = async (idReservation) => {
+    const selectReservationById = `SELECT dateReservation, arrivalDateReservation, departureDateReservation, numberAdult, numberChildren, priceHtReservation, tvaReservation, priceTotal, statusReservation, userId, accommodationId FROM reservations WHERE id = ?`;
+    const [result] = await connexion.query(selectReservationById, [idReservation]);
+    return result;
+}
+
+const updateReservation = async (idReservation, arrivalDateReservation, departureDateReservation, numberAdult, numberChildren, priceHtReservation, tvaReservation, priceTotal, statusReservation, userId, accommodationId) => {
+    const updateReservation = `
+        UPDATE reservations
+        SET arrivalDateReservation = ?, departureDateReservation = ?, numberAdult = ?, numberChildren = ?, priceHtReservation = ?, tvaReservation = ?, priceTotal = ?, statusReservation = ?, userId = ?, accommodationId = ?
+        WHERE id = ?;
+    `;
+    const [result] = await connexion.query(updateReservation, [arrivalDateReservation, departureDateReservation, numberAdult, numberChildren, priceHtReservation, tvaReservation, priceTotal, statusReservation, userId, accommodationId, idReservation]);
+    return result;
+}
+
+const deleteReservation = async (idReservation) => {
+    const deleteReservation = `DELETE FROM reservations WHERE id = ?`;
+    const [result] = await connexion.query(deleteReservation, [idReservation]);
+    return result;
+}
+
 export default {
-    createReservation
+    createReservation,
+    getAllReservations,
+    getReservationById,
+    updateReservation,
+    deleteReservation
 }
--- back/routes/reservationsRoutes.js
+++ back/routes/reservationsRoutes.js
@@ -4,5 +4,9 @@ import reservationsControllers from "../controllers/reservationsControllers.js";
 const router = express.Router();
 
 router.post("/register", reservationsControllers.createReservation);
+router.get("/", reservationsControllers.getAllReservations);
+router.get("/:id", reservationsControllers.getReservationById);
+router.put("/update/:id", reservationsControllers.updateReservation);
+router.delete("/delete/:id", reservationsControllers.deleteReservation);
 
 export default router;
